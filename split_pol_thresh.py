2d5f27bc054c7897f59f218de4e95f05b6b7184f
Fraser Watson <fwatson@nso.edu>
2017-09-11 11:28:27 -0600

    Version update

---
+++ split_pol_thresh.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+def split_pol_thresh(data, threshold, polarity):
+    
+    # This function takes the input data along with a threshold, and returns the same data with everything below the 
+    # threshold removed. In addition, if the data is negative, it is inverted so that the following routines can be 
+    # run on positive and negative data interchangeably.
+    
+    import numpy as np
+    
+    output = np.copy(data)
+    
+    # Check polarity and invert if necessary
+    if polarity == 'pos':
+        pass
+    elif polarity == 'neg':
+        output = output * (-1)
+    else:
+        print('Polarity must be pos or neg.')
+    
+    # Set data below threshold to zero
+    output[output < threshold] = 0
+    
+    return output
+
