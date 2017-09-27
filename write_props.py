192c9ae0cac6bc90dfa807693a8270f8c5cac917
Fraser Watson <fwatson@nso.edu>
2017-09-27 11:20:43 -0600

    Changed to write latitude and longitudes to file using the sunpy map pixel_to_world transformation.

--- write_props.py
+++ write_props.py
@@ -5,14 +5,15 @@
 
 def write_props(props, polarity, image_num, image_date, submap):
     from astropy.coordinates import SkyCoord
+    import astropy.units as u
+    from sunpy.coordinates import frames
     
     filename = '{num:04d}'.format(num = image_num)+polarity+'.txt'
     f = open(filename, 'w')
     for region in props:
-        x = region.centroid[1]
-        y = region.centroid[0]
+        arcsecs_coord = submap.pixel_to_world(region.centroid[1] * u.pix, region.centroid[0] * u.pix)
+        latlong_coord = arcsecs_coord.transform_to(frames.HeliographicStonyhurst)
         area = region.area
-        f.write('{0}, {1:01.1f}, {2:01.1f}\n'.format(image_date, x, y, area))
+        f.write('{0}, {1:01.1f}, {2:01.1f}\n'.format(image_date, latlong_coord.lat.value, latlong_coord.lon.value, area))
     f.close()
-    
 
