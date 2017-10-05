7add5ce3b0cd7f135efde49d6544c32a7d8be8e9
Fraser Watson <fwatson@nso.edu>
2017-10-05 16:51:50 -0600

    Made code PEP8 compliant

--- split_pol_thresh.py
+++ split_pol_thresh.py
@@ -4,16 +4,17 @@
 # In[ ]:
 
 def split_pol_thresh(data, threshold, polarity):
-    
-    # This function takes the input data along with a threshold, and returns the same data with everything below the 
-    # threshold removed. In addition, if the data is negative, it is inverted so that the following routines can be 
-    # run on positive and negative data interchangeably.
-    
+
+    # This function takes the input data along with a threshold, and returns
+    # the same data with everything below the threshold removed. In addition,
+    # if the data is negative, it is inverted so that the following routines
+    # can be run on positive and negative data interchangeably.
+
     import numpy as np
-    
+
     # Create a copy of the data
     output = np.copy(data)
-    
+
     # Check polarity and invert if necessary
     if polarity == 'pos':
         pass
@@ -21,9 +22,9 @@ def split_pol_thresh(data, threshold, polarity):
         output = output * (-1)
     else:
         print('Polarity must be pos or neg.')
-    
+
     # Set data below threshold to zero
     output[output < threshold] = 0
-    
+
     return output
 
