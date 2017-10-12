c0d3053fc87162c8d57a95e7d1ba939ab7d30fd8
Fraser Watson <fwatson@nso.edu>
2017-10-12 09:07:16 -0600

    Added size constraint condition

--- write_props.py
+++ write_props.py
@@ -15,22 +15,24 @@ def write_props(props, polarity, image_num, image_date, submap):
     f = open(filename, 'w')
 
     for region in props:
-        # Get the pixel coordinates from the region.centroid and cast them as
-        # pixels in astropy units. Then, use the sunpy submap to convert those
-        # pixel coordinates to image coordinates (x-arcsecs and y-arcsecs)
-        arcsecs_coord = submap.pixel_to_world(region.centroid[1] * u.pix,
-                                              region.centroid[0] * u.pix)
-        # Now convert the coordinates to Heliographic latitude and longitude as
-        # that is what we need to rotate coordinates in time later on
-        latlong_coord = arcsecs_coord.transform_to(frames.HeliographicStonyhurst)
-        # Record the area (area is NOT deprojected back onto the solar disk)
-        area = region.area
-                
-        # Write the data to the file
-        f.write('{0}, {1:6.1f}, {2:6.1f}, {3:5.0f}\n'.format(image_date,
-                                                     latlong_coord.lat.value,
-                                                     latlong_coord.lon.value,
-                                                     area))
+        # Check to see if the area of the fragment is 12 pixels or larger
+        if region.area >= 12:
+            # Get the pixel coordinates from the region.centroid and cast them as
+            # pixels in astropy units. Then, use the sunpy submap to convert those
+            # pixel coordinates to image coordinates (x-arcsecs and y-arcsecs)
+            arcsecs_coord = submap.pixel_to_world(region.centroid[1] * u.pix,
+                                                  region.centroid[0] * u.pix)
+            # Now convert the coordinates to Heliographic latitude and longitude as
+            # that is what we need to rotate coordinates in time later on
+            latlong_coord = arcsecs_coord.transform_to(frames.HeliographicStonyhurst)
+            # Record the area (area is NOT deprojected back onto the solar disk)
+            area = region.area
+
+            # Write the data to the file
+            f.write('{0}, {1:6.1f}, {2:6.1f}, {3:5.0f}\n'.format(image_date,
+                                                         latlong_coord.lat.value,
+                                                         latlong_coord.lon.value,
+                                                         area))
     # close the file
     f.close()
 
