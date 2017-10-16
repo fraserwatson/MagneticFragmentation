4120206a7b7bcea0d37eb8f6d19b2aae62eeab05
Fraser Watson <fwatson@nso.edu>
2017-10-16 17:26:39 -0600

    Changed to record area and flux in real units

--- write_props.py
+++ write_props.py
@@ -16,7 +16,7 @@ def write_props(props, polarity, image_num, image_date, submap, bulk_path):
     total_flux = 0
     
     # Create filename using the image_num and polarity
-    filename = '/Users/fraser/Github/MagneticFragmentation/fragment_properties/'+'{0}'.format(image_date.strftime('%Y%m%d_%H%M%S'))+'_'+polarity+'.txt'
+    filename = '/Users/fraser/Github/MagneticFragmentationOutput/fragment_properties/'+'{0}'.format(image_date.strftime('%Y%m%d_%H%M%S'))+'_'+polarity+'.txt'
 
     # Open filename for writing
     f = open(filename, 'w')
@@ -34,15 +34,15 @@ def write_props(props, polarity, image_num, image_date, submap, bulk_path):
             # Now convert the coordinates to Heliographic latitude and longitude as
             # that is what we need to rotate coordinates in time later on
             latlong_coord = arcsecs_coord.transform_to(frames.HeliographicStonyhurst)
-            # Record the area (area is NOT deprojected back onto the solar disk)
-            area = region.area
+            # Record the area in sqcm (1 pixel at disk centre is 1.31 million square km) deprojected to disk centre
+            area = region.area * 1.31 * np.power(10, 16) * np.cos(np.deg2rad(latlong_coord.lat.value)) * np.cos(np.deg2rad(latlong_coord.lon.value))
             total_area += area
-            # Record the flux
-            flux = np.sum(region.intensity_image)
+            # Record the flux in Maxwells
+            flux = (np.sum(region.intensity_image) / region.area) * area
             total_flux += flux
 
             # Write the data to the file
-            f.write('{0}, {1:6.1f}, {2:6.1f}, {3:5.0f}\n'.format(image_date,
+            f.write('{0}, {1:6.1f}, {2:6.1f}, {3:20.0f}\n'.format(image_date,
                                                          latlong_coord.lat.value,
                                                          latlong_coord.lon.value,
                                                          area))
@@ -52,7 +52,7 @@ def write_props(props, polarity, image_num, image_date, submap, bulk_path):
     # Open bulk properties file for writing
     g = open(bulk_path, 'a')
     
-    g.write('{0}, {1:4.0f}, {2:5.0f}, {3:8.0f}\n'.format(image_date, total_frags, total_area, total_flux))
+    g.write('{0}, {1:4.0f}, {2:20.0f}, {3:25.0f}\n'.format(image_date, total_frags, total_area, total_flux))
     
     g.close()
 
