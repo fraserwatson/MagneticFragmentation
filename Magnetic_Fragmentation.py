37ef363b883116a37eab7985ac2f722fe23f7609
Fraser Watson <fwatson@nso.edu>
2017-10-16 14:24:48 -0600

    Fixing comments.

--- Magnetic_Fragmentation.py
+++ Magnetic_Fragmentation.py
@@ -92,45 +92,44 @@ for image in range(len(files_to_load)):
     pos_submap_area = data.submap(bl, tr)
     neg_submap_area = data.submap(bl, tr)
 
-#     # Set all of the edge pixels in the submaps to zero. This stops
-#     # errors in the 'find_regions' function
-#     neg_submap_area_data = np.array(neg_submap_area.data)
-#     neg_submap_area_data = np.pad(neg_submap_area_data, ((1, 1), (1, 1)), 'constant', constant_values=(0, 0))
-#     pos_submap_area_data = np.array(pos_submap_area.data)
-#     pos_submap_area_data = np.pad(pos_submap_area_data, ((1, 1), (1, 1)), 'constant', constant_values=(0, 0))
+    # Set all of the edge pixels in the submaps to zero. This stops
+    # errors in the 'find_regions' function
+    neg_submap_area_data = np.array(neg_submap_area.data)
+    neg_submap_area_data = np.pad(neg_submap_area_data, ((1, 1), (1, 1)), 'constant', constant_values=(0, 0))
+    pos_submap_area_data = np.array(pos_submap_area.data)
+    pos_submap_area_data = np.pad(pos_submap_area_data, ((1, 1), (1, 1)), 'constant', constant_values=(0, 0))
     
-#     # Strip out all negative data in the positive submap, and vice versa
-#     pos_submap_data = split_pol_thresh(pos_submap_area_data, threshold, 'pos')
-#     neg_submap_data = split_pol_thresh(neg_submap_area_data, threshold, 'neg')
+    # Strip out all negative data in the positive submap, and vice versa
+    pos_submap_data = split_pol_thresh(pos_submap_area_data, threshold, 'pos')
+    neg_submap_data = split_pol_thresh(neg_submap_area_data, threshold, 'neg')
       
-#     # Find the regions in the positive and negative magnetogram data
-#     pos_region_frame, num_pos_regions = find_regions(pos_submap_data)
-#     neg_region_frame, num_neg_regions = find_regions(neg_submap_data)
+    # Find the regions in the positive and negative magnetogram data
+    pos_region_frame, num_pos_regions = find_regions(pos_submap_data)
+    neg_region_frame, num_neg_regions = find_regions(neg_submap_data)
 
-#     # Convert the data into label frames that scikit-image can use
-#     pos_labeled_frame, pos_num_labels = ms.label(pos_region_frame.astype(int),
-#                                                  return_num=True,
-#                                                  connectivity=2)
-#     neg_labeled_frame, neg_num_labels = ms.label(neg_region_frame.astype(int),
-#                                                  return_num=True,
-#                                                  connectivity=2)
+    # Convert the data into label frames that scikit-image can use
+    pos_labeled_frame, pos_num_labels = ms.label(pos_region_frame.astype(int),
+                                                 return_num=True,
+                                                 connectivity=2)
+    neg_labeled_frame, neg_num_labels = ms.label(neg_region_frame.astype(int),
+                                                 return_num=True,
+                                                 connectivity=2)
 
-#     # Use the scikit-image method 'regionprops' to get various properties on
-#     # the fragments in the image data
-#     pos_properties = ms.regionprops(pos_labeled_frame,
-#                                     intensity_image=pos_submap_area.data)
-#     neg_properties = ms.regionprops(neg_labeled_frame,
-#                                     intensity_image=neg_submap_area.data)
+    # Use the scikit-image method 'regionprops' to get various properties on
+    # the fragments in the image data
+    pos_properties = ms.regionprops(pos_labeled_frame,
+                                    intensity_image=pos_submap_area.data)
+    neg_properties = ms.regionprops(neg_labeled_frame,
+                                    intensity_image=neg_submap_area.data)
         
 
-#     # Write the properties to files. Note that to add new properties,
-#     # do so in the 'write_props' function
-#     write_props(pos_properties, 'p', image, data.date, pos_submap_area, bulk_region_props_path)
-#     write_props(neg_properties, 'n', image, data.date, neg_submap_area, bulk_region_props_path)
+    # Write the properties to files. Note that to add new properties,
+    # do so in the 'write_props' function
+    write_props(pos_properties, 'p', image, data.date, pos_submap_area, bulk_region_props_path)
+    write_props(neg_properties, 'n', image, data.date, neg_submap_area, bulk_region_props_path)
     
-    #pos_submap_area.plot()
+    pos_submap_area.plot()
     imagefilename = 'HMI' + str(image).zfill(4)
-    print(imagefilename)
-    #plt.savefig(imagefilename)
-    #plt.clf()
+    plt.savefig(imagefilename)
+    plt.clf()
 
